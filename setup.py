5fbaa2a2d2e54fecde5f99b175ef515489f68c05
Harel Malka <harel@harelmalka.com>
2015-05-20 23:55:31 +0100

    typo fix in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
 setup(
     name = 'strudelpy',
-    packages = ['strydelpy'],
     version = '0.1',
     description = 'Easy as Pie Emails in Python',
+    long_description='StrudelPy is an easy to use library to manage sending emails in a OO way.'
+                     'The library is comprised of a SMTP object to manage connections to SMTP'
+                     'servers and an Email object to handle the messages themselves.',
     author = 'Harel Malka',
     author_email = 'harel@harelmalka.com',
     url = 'https://github.com/harel/strudelpy',
     download_url = 'https://github.com/harel/strudelpy/archive/0.1.tar.gz',
     keywords = ['email', 'smtp'], # arbitrary keywords
-    classifiers = [],
     license='MIT',
+    packages=['strudelpy', 'strudelpy.tests'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Email',
+        'Programming Language :: Python :: 2.7',
+    ]
 )
